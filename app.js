e93428c9d862dd8b4f4a23335fa728531baad330
john-kodji <johnkodji1@gmail.com>
2017-03-18 10:39:42 -1200

    initial

---
+++ app.js
@@ -0,0 +1,18 @@
+ 'use strict';
+
+ const Readline = require('readline');
+ const rl = Readline.createInterface({
+   input: process.stdin,
+   output: process.stdout,
+   terminal: false
+
+});
+
+
+
+
+rl.setPrompt('Default>')
+rl.prompt();
+rl.on('line', reply => {
+  console.log(`You said ${reply}`);
+});
