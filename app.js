cff1d7eb3ba7317fe02614e6768eb2db001cf143
john-kodji <johnkodji1@gmail.com>
2017-03-18 12:08:42 -1200

    added failsafe

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
  'use strict';
 
  const Readline = require('readline');
- const rl = Readline.createInterface({
+ const rl = Readline.createInterface({    // Terminal interface for the bot
    input: process.stdin,
    output: process.stdout,
    terminal: false
 
 });
-
+const matcher = require('./matcher');   // Call the matcher created. Note the one dot since this is in app.js we call with one dot
 
 
 
 rl.setPrompt('Default>')
 rl.prompt();
 rl.on('line', reply => {
-  console.log(`You said ${reply}`);
+  matcher(reply, data => {    // Matcher replies user from data
+    switch(data.intent) {
+      case 'Hello';
+      console.log("Hey Great... Hope you good?");   // Bots reply
+      rl.prompt
+      break;
+    default: {
+      console.log("Sorry I don't understand:(")   // In case where input isnt in bots data. How it should reply
+    }
+    }
+  }
+
 });
